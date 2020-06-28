03ad2cb0635658489f66da56b973de4a6b638fd6
CardosoPLeo <leonardo_p_c@hotmail.com>
2020-06-28 17:16:12 -0300

    Criacao do banco de dados e configuração da conexão

--- CRUDMVC/Startup.cs
+++ CRUDMVC/Startup.cs
@@ -37,7 +37,9 @@ namespace CRUDMVC
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<CRUDMVCContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("CRUDMVCContext")));
+                options.UseMySql(Configuration.GetConnectionString("CRUDMVCContext"), builder =>
+                builder.MigrationsAssembly("CRUDMVC")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
