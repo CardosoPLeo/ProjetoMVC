923bc4111389c8002aac07be7e9a2e37e6b4c0a7
CardosoPLeo <leonardo_p_c@hotmail.com>
2020-06-28 13:29:48 -0300

    CRUD-Scffolding

---
+++ CRUDMVC/Data/CRUDMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CRUDMVC.Models;
+
+namespace CRUDMVC.Data
+{
+    public class CRUDMVCContext : DbContext
+    {
+        public CRUDMVCContext (DbContextOptions<CRUDMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CRUDMVC.Models.Departamentos> Departamentos { get; set; }
+    }
+}
